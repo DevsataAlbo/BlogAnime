7d6ebd45b69c292039f62eb4beb31d5adcfda555
Juan Rodriguez <satanasdelcolo@gmail.com>
2025-03-27 05:29:18 -0400

    Allowed

--- anime_blog/settings.py
+++ anime_blog/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-default-key-replace-i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 INSTALLED_APPS = [
